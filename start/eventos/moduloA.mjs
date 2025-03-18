bc73a1dded8b67fb27b18556d954e2eeddbff4ba
Luís Eduardo de Paula Albuquerque <144605889+LuisEduardo100@users.noreply.github.com>
2025-03-18 16:18:58 -0300

    Delete start/eventos directory
    fix: pasta eventos já não mais necessária.

--- start/eventos/moduloA.mjs
+++
@@ -1,9 +0,0 @@
-import EventEmitter from "node:events";
-
-const eventEmitter = new EventEmitter();
-
-setTimeout(() => {
-  eventEmitter.emit("eventoDoModuloA", "Dados do Modulo A");
-}, 2000);
-
-export { eventEmitter };
--- start/eventos/moduloB.mjs
+++
@@ -1,5 +0,0 @@
-import { eventEmitter } from "./moduloA.mjs";
-
-eventEmitter.on("eventoDoModuloA", (dados) => {
-  console.log(`Evento recebido no módulo B: ${dados}`);
-});
