337dc70f18b24c3027e53e479e0bfa0ba6f9d598
Luis Eduardo <luisedu.lepa@gmail.com>
2025-03-20 11:04:49 -0300

    feat(http-server): adicionando primeira api que acessa lista de produtos com Node.js

---
+++ web-server/http-server/database/stock.mjs
@@ -0,0 +1,11 @@
+export const stock = [
+  { id: 1, name: "Apple", price: 100, quantity: 2 },
+  { id: 2, name: "Banana", price: 50, quantity: 0 },
+  { id: 3, name: "Orange", price: 75, quantity: 5 },
+  { id: 4, name: "Mango", price: 150, quantity: 0 },
+  { id: 5, name: "Pineapple", price: 125, quantity: 8 },
+  { id: 6, name: "Grapes", price: 80, quantity: 0 },
+  { id: 7, name: "Cherry", price: 175, quantity: 7 },
+  { id: 8, name: "Watermelon", price: 200, quantity: 0 },
+  { id: 9, name: "Strawberry", price: 120, quantity: 6 },
+];
---
+++ web-server/http-server/first-api.mjs
@@ -0,0 +1,35 @@
+import http from "node:http";
+import { stock } from "./database/stock.mjs";
+
+const server = http.createServer();
+
+server.addListener("request", (req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(`
+      <h1>Stock Management System</h1>
+      <ul>
+        <li><a href="/api/stock">Get all stock items</a></li>
+        <li><a href="/api/unavailableStock">Get unavailable stock items</a></li>
+      </ul>
+    `);
+    res.end();
+  }
+
+  if (req.url === "/api/stock") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(JSON.stringify(stock));
+    res.end();
+  } else if (req.url === "/api/unavailableStock") {
+    const unavailableProducts = stock.filter(
+      (product) => product.quantity === 0
+    );
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(JSON.stringify(unavailableProducts));
+    res.end();
+  }
+});
+
+server.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
